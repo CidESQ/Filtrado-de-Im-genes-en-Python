f1f6e7fc721dab9fcb7cf8f8192a0aa02b1cbafe
cidesgo@gmail.com <cidesgo@gmail.com>
2024-08-31 16:57:06 -0600

    Programa listo para ser entregado como tarea

--- EdicionDeImagenes.py
+++ EdicionDeImagenes.py
@@ -15,14 +15,12 @@ import numpy as np
 
 # Abrir el cuadro de dialogo para seleccionar una imagen
 image_path = filedialog.askopenfilename(title='Seleccione la imagen')
-imagen = cv2.imread(image_path)  # Leer imagen seleccionada
-imagen_principal = cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB)
+original_image = cv2.imread(image_path)  # Leer imagen seleccionada
 
-# Separar los canales RGB
-R, G, B = cv2.split(imagen_principal)
-
+# Separar los canales RGB (BGR en OpenCV) para aplicar la convolucion en cada uno y despues volverlos a unir.
+B, G, R = cv2.split(original_image)
 '''
- CREACION DE KERNELS/FILTROS
+ DEFINICION DE KERNELS/FILTROS
 '''
 kernel_1 = np.ones((3, 3), np.float32) / 9  # Blur / media
 kernel_2 = np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]], np.float32) / 16  # Gausiano
@@ -31,35 +29,31 @@ kernel_4 = np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]], np.float32)  # Laplacian
 kernel_5 = np.array([[-2, -1, 0], [-1, 1, 1], [0, 1, 2]], np.float32)  # Filtro Emboss
 kernel_6 = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]], np.float32)  # Filtro High-Pass
 kernel_7 = np.array([[1, 0], [0, -1]], np.float32)  # Filtro Robert Cross
-
-
 # Convolcion de kernels: kernel_combinado = convolve2d(kernel_a, kernel_b, mode='full')
-# Funcion
-def aplicar_kernel(kernel):
-    r_kernel = cv2.filter2D(R, -1, kernel)
+
+
+def apply_kernel(kernel):
+    b_kernel = cv2.filter2D(B, -1, kernel)  # ? aplicar un filtro de convolución a un canal
     g_kernel = cv2.filter2D(G, -1, kernel)
-    b_kernel = cv2.filter2D(B, -1, kernel)
-    imagen_filtrada_rgb = cv2.merge((r_kernel, g_kernel, b_kernel))
-    # imagen_filtrada_bgr = cv2.cvtColor(imagen_filtrada_rgb, cv2.COLOR_RGB2BGR)  # mostrar con OpenCV
-    return imagen_filtrada_rgb
+    r_kernel = cv2.filter2D(R, -1, kernel)
+    filtered_image = cv2.merge((b_kernel, g_kernel, r_kernel))
+    return filtered_image
 
 
-def interfaz_2():
-    cv2.imshow('Imagen Original', imagen)
-    cv2.imshow('Filtro Blur', cv2.cvtColor(aplicar_kernel(kernel_1), cv2.COLOR_RGB2BGR))
-    cv2.imshow('Filtro Gausiano', cv2.cvtColor(aplicar_kernel(kernel_2), cv2.COLOR_RGB2BGR))
-    cv2.imshow('Sharpening', cv2.cvtColor(aplicar_kernel(kernel_3), cv2.COLOR_RGB2BGR))
-    cv2.imshow('Laplaciano', cv2.cvtColor(aplicar_kernel(kernel_4), cv2.COLOR_RGB2BGR))
-    cv2.imshow('Emboss', cv2.cvtColor(aplicar_kernel(kernel_5), cv2.COLOR_RGB2BGR))
-    cv2.imshow('High pass', cv2.cvtColor(aplicar_kernel(kernel_6), cv2.COLOR_RGB2BGR))
-    cv2.imshow('Robert Cross', cv2.cvtColor(aplicar_kernel(kernel_7), cv2.COLOR_RGB2BGR))
-    # Con el teclado pasamos a la imagen
-    cv2.waitKey(0)
+def show_filters():
+    cv2.imshow('Imagen Original', original_image)
+    cv2.imshow('1 Filtro Blur', apply_kernel(kernel_1))
+    cv2.imshow('2 Filtro Gausiano', apply_kernel(kernel_2))
+    cv2.imshow('3 Sharpening', apply_kernel(kernel_3))
+    cv2.imshow('4 Laplaciano', apply_kernel(kernel_4))
+    cv2.imshow('5 Emboss', apply_kernel(kernel_5))
+    cv2.imshow('6 High pass', apply_kernel(kernel_6))
+    cv2.imshow('7 Robert Cross', apply_kernel(kernel_7))
+    cv2.waitKey(0)  # ? Terminar programa con tecla '0'
     cv2.destroyAllWindows()
 
 
-# interfaz_1()
-interfaz_2()
+show_filters()
 
 '''
 Explicacion filtros/kernels
